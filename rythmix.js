e7124d9ef8530af57ddce5f28cd948a66ee51ebf
Im-Tarun <119149481+Im-Tarun@users.noreply.github.com>
2024-03-07 18:42:01 +0530

    ww

--- rythmix.js
+++ rythmix.js
@@ -3,9 +3,19 @@
 // document.addEventListener('contextmenu', function (e) {
 //     e.preventDefault();
 // });
+function convertSecondsToMinsSecs(sec) {
+    let min = Math.floor(sec / 60);
+    let seconds = sec % 60;
 
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
 
-async function getSongs(){
+    return `${min}:${seconds}`;
+  }
+  
+
+async function main(){
     //getting songs from the folder
     const response =  await fetch('http://127.0.0.1:5500/musicClone/songs/');
     const data = await response.text();
@@ -13,18 +23,12 @@ async function getSongs(){
     div.innerHTML = data
     
     const all_a = div.querySelectorAll('a');
-    const selectSongs = Array.from(all_a)
-    .filter( elem => elem.href.endsWith('.mp3'))
+    const selectSongs = Array.from(all_a).filter( elem => elem.href.endsWith('.mp3'))
 
     const songs = selectSongs.map(element => element.href)
     
     const songName = selectSongs.map(element => {
-      let i = element.title
-      .replaceAll("(MP3_320K).mp3", "")
-      .replaceAll("(MP3_160K).mp3", "")
-      .replaceAll(".mp3", "")
-      .replaceAll("_", "")
-      .replaceAll("Video", '')
+      let i = element.title.replaceAll("(MP3_320K).mp3", "").replaceAll("(MP3_160K).mp3", "").replaceAll(".mp3", "").replaceAll("_", "").replaceAll("Video", '')
       if (i.length > 64) {
         i = i.substring(0, 64) + '...';
       }
@@ -37,7 +41,7 @@ async function getSongs(){
     
     songName.forEach(element => {
         const songBox = document.createElement("div")
-        songBox.setAttribute("class","song-box");
+        songBox.setAttribute("class","song-box pointer");
         songBox.innerHTML = 
         `<div class="song-icon"><i class='bx bx-music'></i></div>
         <div class="song-info">${element}</div>
@@ -58,10 +62,22 @@ async function getSongs(){
             currentAudio = new Audio(songs[index]);
             currentAudio.play();
 
-            let currentSongName = songName[index];
-            document.querySelector('.current-song').innerHTML = currentSongName
-
+            document.querySelector('.current-song').innerHTML = songName[index];
+            
             document.querySelector('.controller-box').style.display= "block" ;
+
+            playPause.innerHTML = `<i class='bx music-icons bx-pause'></i>`
+
+            currentAudio.addEventListener('timeupdate',()=>{
+                let songDuration = convertSecondsToMinsSecs(Math.round(currentAudio.duration)); 
+                let currentTime = convertSecondsToMinsSecs(Math.round(currentAudio.currentTime)) 
+                document.querySelector('.song-time').innerHTML =`${currentTime} / ${songDuration}`;
+                
+                document.querySelector('.sound-bar-fill').style.width = ((currentAudio.currentTime / currentAudio.duration )*100 ) + 1 + "%"
+                document.querySelector('.progress-control-handle').style.left = (currentAudio.currentTime / currentAudio.duration )*100 + "%"
+            })
+            
+             
         });
     });
     
@@ -77,11 +93,24 @@ async function getSongs(){
             currentAudio.pause()
             playPause.innerHTML = `<i class='bx bx-play music-icons'></i> `
         }
+        
     })
+
+    document.querySelector('.progress-control-bar').addEventListener( 'click', function(e) {
+        currentAudio.currentTime = (e.offsetX / e.target.clientWidth) * currentAudio.duration;
+        //e is event 
+        // console.log(e.target.clientWidth); give width of element in px 
+        
+    });
     
+
+
+
+    
+
     // 5:35:38 give play pause button functionality
     // givce folder and click on it to ope all songs in it
 
    
 }
-getSongs()
+main()
